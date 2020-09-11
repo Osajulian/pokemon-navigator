ab638b67626ed53538b60488b35616329aeb7d46
Osa Julian <osajulian@hotmail.com>
2020-09-11 14:50:15 -0400

    Updates

---
+++ precache-manifest.baf8b65098ec41f675df0c77f7541925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ee0d33c509a30f32c52705affbcc4f",
+    "url": "/pokemon-navigator/index.html"
+  },
+  {
+    "revision": "e66a7cf065b4a01ca2a1",
+    "url": "/pokemon-navigator/static/css/main.78127274.chunk.css"
+  },
+  {
+    "revision": "ac57eba5f60d6f211efb",
+    "url": "/pokemon-navigator/static/js/2.f33df023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-navigator/static/js/2.f33df023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66a7cf065b4a01ca2a1",
+    "url": "/pokemon-navigator/static/js/main.cd8a4e74.chunk.js"
+  },
+  {
+    "revision": "4a83e1b2d1c9980590e4",
+    "url": "/pokemon-navigator/static/js/runtime-main.c1e9ea45.js"
+  }
+]);
